159c81b1cdc3ef00a3783941c2235cf523b5c8c3
NataliaDavida <84465903+NataliaDavida@users.noreply.github.com>
2022-06-18 10:40:12 +0300

    Update weather.component.ts

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -37,36 +37,11 @@ export class WeatherComponent implements OnInit {
           });
           OpenStreetMap_Mapnik.addTo(this.mymap)
 
-          // snow
-          const OpenSnowMap_pistes = L.tileLayer('https://tiles.opensnowmap.org/pistes/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors & ODbL, &copy; <a href="https://www.opensnowmap.org/iframes/data.html">www.opensnowmap.org</a> <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
-          });
-       
-          // wind
-          const OpenWeatherMap_Wind = L.tileLayer('http://{s}.tile.openweathermap.org/map/wind/{z}/{x}/{y}.png?appid={apiKey}', {
-            maxZoom: 19,
-            attribution: 'Map data &copy; <a href="http://openweathermap.org">OpenWeatherMap</a>',
-            apiKey: '<ACCESS_TOKEN=pk.eyJ1IjoibmF0YWRhdiIsImEiOiJja3d3Ymhybm8wMXp4MnByMDB6Y2l0dXR1In0.7gPxtqqKR-PvyxNSjOwbkw>',
-            opacity: 0.5
-          });
-
-          // clouds
-          const OpenWeatherMap_Clouds = L.tileLayer('http://{s}.tile.openweathermap.org/map/clouds/{z}/{x}/{y}.png?appid={apiKey}', {
-            maxZoom: 19,
-            attribution: 'Map data &copy; <a href="http://openweathermap.org">OpenWeatherMap</a>',
-            apiKey: '<ACCESS_TOKEN>',
-            opacity: 0.5
-          });
-
           // marker
           const marker = L.marker([this.lat, this.lon]).addTo(this.mymap);
 
           const baseMaps = {
-            "Osm": OpenStreetMap_Mapnik,
-            "Snow": OpenSnowMap_pistes,
-            "Wind": OpenWeatherMap_Wind,
-            "Clouds": OpenWeatherMap_Clouds
+            "Osm": OpenStreetMap_Mapnik
         };
     
             const overlayMaps = {
@@ -80,9 +55,6 @@ export class WeatherComponent implements OnInit {
   });
 
   }
- 
-
-
    getLocation(){
      if('geolocation' in navigator) {
        navigator.geolocation.watchPosition((success)=>{
@@ -97,7 +69,6 @@ export class WeatherComponent implements OnInit {
        })
      }
    }
-  
    getCity(city: string) {
      this.weatherService.getCoords(city).subscribe((data: any)=> {
        this.weather = data;
